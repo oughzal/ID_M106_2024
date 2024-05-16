c501281fe50688e7ae8f7f0a0a3f48e3431d7f0a
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-05-16 23:16:37 +0100

    Commit

--- ID101-S04/carre.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Donner N :"))
-if n%2 ==0 : n +=1
-M =[[0 for i in range(n)] for j in range(n)]
-
-# code
-for i in range(n):
-    for j in range(n):
-        print(M[i][j],end="\t")
-    print()
