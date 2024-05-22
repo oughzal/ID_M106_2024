1cc1b3515b79c59f6637536a483558616cd1e8c2
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-05-22 21:01:16 +0100

    commit

--- ID101-S01/Exercice01.py
+++ ID101-S01/Exercice01.py
@@ -1,16 +1,5 @@
-# // Entrée
-# Ecrire("Donner un nombre : ")
-# Lire(N)
-N = float(input("Donner un nombre : ") )  # int(input(""))
-# //Traitement
-# C = N^2
+N = float(input("Donner un nombre : "))
 C = N**2
-# //Sortie
-# Ecrireln(N,"^2 = " , C)
-# Ecrire("Le carré de ", N , " est ", C)
 print(N,"^2 =" , C)
 print("Le carré de ", N , " est ", C)
-
-# // Traitement + Sortie
-# Ecrire ("le carré de ", N, " égale à ", N*N)
 print("le carré de ", N, " égale à ", N*N)
